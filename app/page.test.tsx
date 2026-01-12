4db02aef6ac69e42dc0da7c681cfbf7016924dae
AudimaOks <administrator-ti@AudimaOks-2.local>
2026-01-12 15:12:29 +0700

    4. Button Toggle and Component View Check (Page.test.tsx)

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Page from "./page";
 
@@ -11,5 +12,66 @@ describe("HomaPage", () => {
 
     expect(heading).toHaveTextContent("Welcome to Readle"); //Fungsinya yaitu untuk mengecek apakah ada teks "Welcome to Readle" di dalam heading
     expect(heading).toHaveTextContent(/welcome/i); //Fungsinya yaitu untuk mengecek apakah ada kata welcome di dalam heading, sedangkan "i" pada akhir regex berarti pengecekan ini tidak peka terhadap huruf besar/kecil
+  });
+
+  it('homepage can toggle "Show More" and "Show Less" button', async () => {
+    const user = userEvent.setup();
+
+    render(<Page />);
+
+    const button = screen.getByRole("button", { name: /show more/i });
+
+    // Klik pertama: tombol berubah jadi "Show Less"
+    await user.click(button);
+    // expect(button).toHaveTextContent("Show Less");
+    expect(button).toHaveTextContent(/show less/i);
+
+    // Klik kedua: tombol kembali jadi "Show More"
+    await user.click(button);
+    expect(button).toHaveTextContent("Show More");
+  });
+
+  it('should show Another Card when "Show More" is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<Page />);
+
+    // Pastikan isExpanded awalnya false, jadi elemen dengan aria-label "more features" tidak ada
+    const moreFeatures = screen.queryByLabelText("more features");
+    expect(moreFeatures).not.toBeInTheDocument();
+
+    // Klik tombol "Show More"
+    const firstButton = screen.getByRole("button", { name: /show more/i });
+    await user.click(firstButton);
+
+    const moreFeaturesAfterClick = screen.getByRole("list", {
+      name: /more features/i,
+    });
+    expect(moreFeaturesAfterClick).toBeInTheDocument();
+  });
+
+  it('should hide Additional Cards when "Show Less" is clicked', async () => {
+    const user = userEvent.setup();
+
+    render(<Page />);
+
+    // Pastikan isExpanded awalnya false, jadi elemen dengan aria-label "more features" tidak ada
+    const moreFeatures = screen.queryByLabelText("more features");
+    expect(moreFeatures).not.toBeInTheDocument();
+
+    // Klik tombol "Show More"
+    const firstButton = screen.getByRole("button", { name: /show more/i });
+    await user.click(firstButton);
+
+    const moreFeaturesAfterClick = screen.getByLabelText("more features");
+    expect(moreFeaturesAfterClick).toBeInTheDocument();
+
+    // Klik tombol "Show Less"
+    const secondButton = screen.getByRole("button", { name: /show less/i });
+    await user.click(secondButton);
+
+    const moreFeaturesAfterSecondClick =
+      screen.queryByLabelText("more features");
+    expect(moreFeaturesAfterSecondClick).not.toBeInTheDocument();
   });
 });
